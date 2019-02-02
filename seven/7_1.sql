beddfc9b4a1803cdfdaac72261e3e3ed101288a8
Evan Allen <evanallen13@gmail.com>
2019-02-02 13:14:33 -0800

    added Seven

---
+++ seven/7_1.sql
@@ -0,0 +1,8 @@
+set echo off
+set feedback off
+set verify off
+set heading on
+
+/* start C:\Users\evana\Desktop\IS480\seven\7_1.sql */
+
+/* Display the Account Balance if balance is less than 0 display as 0 */
---
+++ seven/tables.sql
@@ -0,0 +1,22 @@
+set echo on
+set feedback on
+set verify on
+set heading on
+
+/* start C:\Users\evana\Desktop\IS480\seven\tables.sql */
+drop table customers;
+
+create table Customers(
+	Cnum number(3) primary key,
+	Cname varchar2(15),
+	accountBalance number(8,2),
+	totalMileage number(6));
+
+insert into Customers values (101,'Andy',85.25,2152);
+insert into Customers values (102,'Betty',170,808);
+insert into Customers values (103,'Cindy',-55.13,31);
+insert into Customers values (104,'David',1308.02,5510);
+insert into Customers values (105,'Ellen',99.77,11154);
+insert into Customers values (106,'Frank',-220.48,380);
+
+
