9f63eeee319ead50822815831dfe95f030719b28
Evan Allen <evanallen13@gmail.com>
2019-02-07 15:17:55 -0800

    fixed drop all

--- drop.sql
+++ drop.sql
@@ -2,16 +2,5 @@
 /* start C:\Users\evana\Desktop\IS480\drop.sql */
 
 
-drop table CUSTOMERS;
-drop table MAJORS;
-drop table STUDENTS;
-drop table COURSES;
-drop table SCHCLASSES;
-drop table ENROLLMENTS;
-drop table STU;
-drop table AMAZON;
-drop table ACCOUNTS;
-drop table STUDENTEMAIL;
-
 
 select table_name from user_tables;
